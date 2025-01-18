8e83ad1b978d79a2bb12548b75313401abb8487d
ahmedmudass <131138880+ahmedmudass@users.noreply.github.com>
2025-01-18 11:05:08 +0500

    1/18/2025 work

--- practice_work/src/App.js
+++ practice_work/src/App.js
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Marksheet from './Component/Marksheet';
+import Quiz_app from './Component/Quiz_app';
 
 function App() {
   return (
     <div className="App">
-      < Marksheet />
+      {/* < Marksheet /> */}
+      < Quiz_app /> 
     </div>
   );
 }
